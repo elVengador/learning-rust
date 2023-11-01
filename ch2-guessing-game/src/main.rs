226dd46988ae186e102dfeebd13312cbc3064b22
elVengador <dev.jimynicanor1001@gmail.com>
2023-11-01 11:45:18 -0500

    add explanation in ch2-guessing-game

--- ch2-guessing-game/src/main.rs
+++ ch2-guessing-game/src/main.rs
@@ -1,10 +1,11 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+use rand::Rng;           // bring random library into the scope
+use std::cmp::Ordering;  // bring comparator library into the score
+use std::io;             // bring the io library into the scope
 
 fn main() {
     println!("Guess the number!");
 
+    // define a immutable variable
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("the secret_number is: {secret_number}");
@@ -12,15 +13,21 @@ fn main() {
     loop {
         println!("Please input your guess.");
 
+        // define a mutable variable
+        // call the new() function associated to the type String
         let mut guess = String::new();
 
+        // create an instance of stdin
         io::stdin()
+            // send a mutable reference to read_line method
             .read_line(&mut guess)
+            // handing error
             .expect("Failed to read line");
 
+        // match expression, will execute the expression will fit the condition
         let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+            Ok(num) => num, // callback when operation is successful
+            Err(_) => continue,  // callback when operation has an error
         };
 
         match guess.cmp(&secret_number) {
@@ -32,6 +39,7 @@ fn main() {
             }
         }
 
+        // print the guess variable
         println!("You guessed: {guess}");
     }
 }
